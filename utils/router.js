d43a0f541f1eefd6eda035a6ef8cadbf5616be14
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2023-10-13 18:32:29 +0300

    Update: sabmin navigate

--- utils/router.js
+++ utils/router.js
@@ -3,6 +3,8 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { LoginScreen } from "../Screens/auth/LoginScreen";
 import { RegisterScreen } from "../Screens/auth/RegisterScreen";
 import { Home } from "../Screens/main/Home";
+import { CommentsScreen } from "../Screens/main/CommentsScreen";
+import { MapScreen } from "../Screens/main/MapScreen";
 
 const MainStack = createStackNavigator();
 
@@ -21,9 +23,17 @@ export const defineRoute = (isAuth) => {
             component={RegisterScreen}
             options={{ headerShown: false }}
           />
+          <MainStack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
         </>
       ) : (
-        <MainStack.Screen name="Home" component={Home} />
+        <>
+          <MainStack.Screen name="Comments" component={CommentsScreen} />
+          <MainStack.Screen name="Map" component={MapScreen} />
+        </>
       )}
     </MainStack.Navigator>
   );

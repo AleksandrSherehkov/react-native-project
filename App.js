090845ebb97768076eace4910e73b4848086718e
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2023-10-19 15:37:35 +0300

    Update fix_bug

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import "react-native-gesture-handler";
-import { Text } from "react-native";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { useFonts } from "expo-font";
+import 'react-native-gesture-handler';
+import { Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { useFonts } from 'expo-font';
 
-import { persistor, store } from "./redux/store";
-import { AppNavigation } from "./components/AppNavigation";
+import { persistor, store } from './redux/store';
+import { AppNavigation } from './components/AppNavigation';
 
 export default function App() {
   const [fontsLoaded, error] = useFonts({
-    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
-    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
-    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
   });
 
   if (!fontsLoaded && !error) {
@@ -21,6 +21,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
+        <StatusBar animated={true} barStyle="dark-content" backgroundColor="#fff" />
         <AppNavigation />
       </PersistGate>
     </Provider>
